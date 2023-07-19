ea8d0d8e43636b122feea74bf3c51d78bae4a69e
sakusan <sssaku.039@gmail.com>
2023-07-19 05:16:19 +0000

    Update Top.jsx and UserProvider.jsx

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
 import { useHistory } from "react-router-dom";
 
 import { SecondaryButton } from "../atoms/button/SecondaryButton";
+import { UserContext } from "../../providers/UserProvider";
 
 export const Top = () => {
   const history = useHistory();
+  const { setUserInfo } = useContext(UserContext);
+
   const onClickAdmin = () =>
     history.push({ pathname: "/users", state: { isAdmin: true } });
   const onClickGeneral = () =>
