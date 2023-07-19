766d8aa74232576df9ac1cc85083590e9749bee9
sakusan <sssaku.039@gmail.com>
2023-07-19 09:15:55 +0000

    Update Top.jsx

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -9,10 +9,16 @@ export const Top = () => {
   const history = useHistory();
   const { setUserInfo } = useContext(UserContext);
 
-  const onClickAdmin = () =>
-    history.push({ pathname: "/users", state: { isAdmin: true } });
-  const onClickGeneral = () =>
-    history.push({ pathname: "/users", state: { isAdmin: false } });
+  const onClickAdmin = () => {
+    setUserInfo({ isAdmin: true });
+    history.push("/users");
+  };
+
+  const onClickGeneral = () => {
+    setUserInfo({ isAdmin: false });
+    history.push("/users");
+  };
+
   return (
     <SContainer>
       <h2>TOPページです</h2>
